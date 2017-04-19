d8d1f1556191d5102c3f817ec0149173fae16a66
zhaoshiwei1 <447518646@qq.com>
2017-04-19 11:21:34 +0800

    complete database selection utility from specific BD/TAB

---
+++ src/com/api/client/globaldefine/GlobalDefine.java
@@ -0,0 +1,18 @@
+package com.api.client.globaldefine;
+
+public class GlobalDefine 
+{
+	/*
+	 * 定义了数据库测试用例表中每条测试的固定定义字段名
+	 */
+	public static final String TC_ID = "TESTCASEID";
+	public static final String TC_NAME = "NAME";
+	public static final String TC_URL = "URL";
+	public static final String TC_METHOD = "HTTP_METHOD";
+	
+	/*
+	 * 定义sqlite3 数据库文件地址和表名称
+	 */
+	public static final String DB_PATH = "F:/backend-api-database/testCase.db3";
+	public static final String TC_TABLE = "TESTCASE";
+}
--- src/com/api/client/main/Hello.java
+++ src/com/api/client/main/Hello.java
@@ -6,43 +6,24 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 import java.sql.Types;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.api.client.db.DataBaseUtility;
+import com.api.client.globaldefine.GlobalDefine;
 
 public class Hello 
 {
 	public static void main(String[] args)
 	{
-		System.out.print("Hello world!");
-		
-		Connection c = null;
-		Statement s = null;
-		try {
-		      Class.forName("org.sqlite.JDBC");
-		      c = DriverManager.getConnection("jdbc:sqlite:"+"F:/backend-api-database/testCase.db3");
-		      c.setAutoCommit(false);
-		      System.out.println("Opened database successfully");
-
-		      s = c.createStatement();
-		      ResultSet rs = s.executeQuery( "SELECT * FROM TESTCASE;" );
-		      while ( rs.next() ) {
-		    	  ResultSetMetaData metaData = rs.getMetaData();  
-		          for (int i = 0; i < metaData.getColumnCount(); i++) {  
-		              // resultSet数据下标从1开始  
-		              String columnName = metaData.getColumnName(i + 1);  
-//		              int type = metaData.getColumnType(i + 1);  
-//		              if (Types.INTEGER == type) {  
-//		                  // int  
-//		              } else if (Types.VARCHAR == type) {  
-//		                  // String  
-//		              }  
-		              System.out.print(columnName + "\t");  
-		          }  
-		      }
-		      rs.close();
-		      s.close();
-		      c.close();
-		    } catch ( Exception e ) {
-		      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-		      System.exit(0);
-		    }
+		List<List<Map>> l = new ArrayList();
+		System.out.print("Hello world!"+"\n");
+		DataBaseUtility dbu = new DataBaseUtility(GlobalDefine.DB_PATH);
+		l = dbu.getData(GlobalDefine.TC_TABLE);
+		for (List m : l)
+		{
+			System.out.print(m.toString()+"\n");
+		}
 	}
 }
